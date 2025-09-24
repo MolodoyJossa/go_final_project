1cf27756b0be107b598b27487a74442b8c3722db
MolodoyJossa <73858821+MolodoyJossa@users.noreply.github.com>
2025-09-25 01:39:04 +0300

    create database

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"go_final_project-main/pkg/db"
+	"go_final_project-main/pkg/server"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
-
-	"go_final_project-main/server"
 )
 
 func main() {
 
 	if err := godotenv.Load(); err != nil {
 		log.Println(".env не найден или не загружен")
+	}
+
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+
+	if err := db.Init(dbFile); err != nil {
+		log.Fatalf("Ошибка инициализации БД: %v", err)
 	}
 
 	server.Start()
